0819897892e76f91e47dc7f64f71e79e420b07e2
sushma <sushmareddy.g526@gmail.com>
2025-01-10 18:07:52 +0530

    this is second commit

---
+++ src/kite.java
@@ -0,0 +1,2 @@
+public class kite {
+}
--- src/lab002.java
+++ src/lab002.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class lab002 {
+    public static void main(String[] args) {
+        System.out.println("this lab002");
+    }
 }
